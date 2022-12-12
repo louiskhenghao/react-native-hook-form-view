2609942011f05808c3a4013f3be8405e09c8b27c
louiskhenghao <louiskhenghao@gmail.com>
2022-12-12 14:37:41 +0800

    feat: expose submit method for form submission from useNativeFormContext hook

--- src/context/NativeFormContext/props.ts
+++ src/context/NativeFormContext/props.ts
@@ -4,3 +4,10 @@ export type ContextProps<T = any, F extends FieldValues = FieldValues> = {
   form?: UseFormReturn<F, any>;
   submitHandler?: (values: F) => void;
 };
+
+export type ContextHookProps<
+  T = any,
+  F extends FieldValues = FieldValues
+> = Pick<ContextProps<T, F>, "form"> & {
+  submit?: () => void;
+};
